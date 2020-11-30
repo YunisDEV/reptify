5320d0e17787f99b3aa569055ecc0c8d18397832
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-11-30 19:20:33 +0400

    v 0.0.1

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md","r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="reptilia",
+    name="reptify",
     version="0.0.1",
     author="Yunis Huseynzade",
     author_email="yunisdev.04@gmail.com",
     description="Frontend library for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/YunisDEV/reptilia",
+    url="https://github.com/YunisDEV/reptify",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
